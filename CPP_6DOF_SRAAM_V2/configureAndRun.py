a28444940305368c121c9eb59814f228ee357b9a
Graham Beech <grahambeech5833@gmail.com>
2022-12-04 18:45:34 -0600

    deleted output and caches -G

--- CPP_6DOF_SRAAM_V2/configureAndRun.py
+++ CPP_6DOF_SRAAM_V2/configureAndRun.py
@@ -7,13 +7,13 @@ ballistic = 0 # Boolean.
 INTEGRATION_METHOD = 1 # 0 = Euler, 1 = RK2, 2 = RK4.
 phi = 0 # Degrees.
 theta = 45 # Degrees.
-psi = 25 # Degrees.
+psi = 0 # Degrees.
 posE = 0 # Meters.
 posN = 0 # Meters.
 posU = 0 # Meters.
-tgtE = 5000 # Meters.
+tgtE = 3000 # Meters.
 tgtN = 2000 # Meters.
-tgtU = 5000 # Meters.
+tgtU = 3000 # Meters.
 LogData = 1 # Boolean.
 ConsoleReport = 1 # Boolean.
 ID = "missileRk2"
